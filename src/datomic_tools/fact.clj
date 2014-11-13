fbe999c68bcdca4f6141b593ae04833652b279ca
priyatam <priyatam@gmail.com>
2014-11-12 23:45:00 -0800

    fixed issues with add/retract

--- src/datomic_tools/fact.clj
+++ src/datomic_tools/fact.clj
@@ -5,25 +5,44 @@
             [clojure.string :as str]
             [clojure.edn :as edn]
             [datomic.api :as d]
-            [datomic-tools.peer :refer [conn]]
+            [datomic-tools.db :refer [conn]]
+            [datomic-tools.query :as query]
             [datomic-tools.utils :refer :all])
   (:import datomic.Util))
 
 
-(defn add [attr-data]
-  "Add a fact from attribute data"
-  (let [id {:db/id (d/tempid :db.part/user)}
-        tx-data (conj id attr-data)]
-    (d/transact @conn (vector tx-data))))
+(defn temp-id []
+  (d/tempid :db.part/user))
 
-(defn retract [attr-data]
+
+(defn install
+  "Install txdata and return the single new entity possessing attr"
+  [txdata attr]
+  (let [t  (d/basis-t (:db-after @(d/transact @conn txdata)))
+        db (d/db @conn)]
+    (query/find-entity '[:find ?e
+                         :in $ ?attr ?t
+                         :where [?e ?attr _ ?t]]
+                       db (d/entid db attr) (d/t->tx t))))
+
+(defn add
+  "Add a fact as a set of attr/val pairs"
+  ([attr-data]
+   "Add a fact from attribute data"
+   (let [id {:db/id (temp-id)}
+         tx-data (conj id attr-data)]
+     (d/transact @conn (vector tx-data))))
+  ([id attr-data]
+    (let [tx-data (conj id attr-data)]
+      (d/transact @conn (vector tx-data)))))
+
+(defn retract [id attr-data]
   "Add a fact from attribute data"
-  (let [op {:db/retract (d/tempid :db.part/user)}
-        tx-data (conj op attr-data)]
+  (let [op :db/retract
+        tx-data (conj op id attr-data)]
     (d/transact @conn (vector tx-data))))
 
 (defn delete-tx
   "Returns transactable data for delete."
   [ids]
   (map #(vec [:db.fn/retractEntity (Long. %)]) ids))
-
