9a5ceb999bf3e8da98a6aabb53b073d3befb1058
priyatam <priyatam@gmail.com>
2014-11-12 23:46:42 -0800

    refactor

---
+++ test/datomic_tools/db_test.clj
@@ -0,0 +1,14 @@
+(ns datomic-tools.db-test
+  (:require [clojure.test :refer :all]
+            [datomic-tools.db :refer :all]))
+
+(deftest test-db
+  (testing "setup"
+    (let [res (setup :mem "test")]
+      (is (contains? res "datomic:mem://test"))))
+  (testing "playground"
+    (let [res (playground)]
+      (is (contains? res "datomic:mem://"))))
+  (testing "cleanup!"
+    (let [data (delete! :mem "test")]
+      (is (contains? data :done)))))
