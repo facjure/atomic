436bcb0fa3f3dd2f5fdf8de58f47e819ce04f59d
Priyatam Mudivarti <priyatam@gmail.com>
2015-05-07 20:56:04 -0700

    added handy utilities
    credit: https://github.com/zcaudate/adi/blob/master/src/adi/core/types.clj

--- src/atomic/utils.clj
+++ src/atomic/utils.clj
@@ -21,27 +21,49 @@
    Usage:
        fetch :resource 'data/poem.txt'"
   (cond
-   (= io-type :str) (str file)
-   (= io-type :resource) (slurp (io/resource file))
-   (= io-type :url) (slurp (io/as-url file))
-   (= io-type :file) (slurp (io/as-file file))
-   :else nil))
+    (= io-type :str) (str file)
+    (= io-type :resource) (slurp (io/resource file))
+    (= io-type :url) (slurp (io/as-url file))
+    (= io-type :file) (slurp (io/as-file file))
+    :else nil))
 
 (defn int-to-base62
   "Convert an integer to base62"
-   ([n] (int-to-base62 (rem n 62) (quot n 62) ""))
-   ([remainder number accum]
-    (let [alphabet "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"]
-      (cond
-        (zero? number) (str (nth alphabet remainder) accum)
-        :else (recur
-               (rem number 62)
-               (quot number 62)
-               (str (nth alphabet remainder) accum))))))
+  ([n] (int-to-base62 (rem n 62) (quot n 62) ""))
+  ([remainder number accum]
+   (let [alphabet "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+     (cond
+       (zero? number) (str (nth alphabet remainder) accum)
+       :else (recur
+              (rem number 62)
+              (quot number 62)
+              (str (nth alphabet remainder) accum))))))
 
 (defn gen-short-url []
   "Generate a unique, short URL, with 8 chars"
-   (subs
-    (int-to-base62
-     (java.math.BigInteger. (str/replace (str (java.util.UUID/randomUUID)) "-" "") 16))
-    0 8))
+  (subs
+   (int-to-base62
+    (java.math.BigInteger. (str/replace (str (java.util.UUID/randomUUID)) "-" "") 16))
+   0 8))
+
+(defn last-transaction-time [db]
+  (let [t (-> db d/basis-t)]
+    [t (ffirst (d/q '[:find ?t
+                      :in $ ?tx
+                      :where [?tx :db/txInstant ?t]]
+                    db
+                    (d/t->tx t)))]))
+
+(defmethod print-method datomic.peer.LocalConnection
+  [conn w]
+  (.write w  (if-let [db (try (d/db conn)
+                              (catch Exception t))]
+               (let [[t dt] (last-transaction-time db)]
+                 (format "#connection%s" {t dt}))
+               "#connection{}")))
+
+(defmethod print-method datomic.db.Db
+  [db w]
+  (.write w (if-let [[t dt] (last-transaction-time db)]
+              (format "#db%s" {t dt})
+              "#db{}")))
