3fd5ad740f362ec59e0b42b6806c6af8d8451e45
Priyatam Mudivarti <priyatam@gmail.com>
2015-05-13 14:17:07 -0700

    init rules

---
+++ src/atomic/rules.clj
@@ -0,0 +1,12 @@
+(ns atomic.rules
+  (:refer-clojure :exclude [name])
+  (:require [environ.core :refer [env]]
+            [clojure.pprint :as pp]
+            [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.edn :as edn]
+            [datomic.api :as d]
+            [clojure.tools.logging :as log]
+            [atomic.db :as db]
+            [atomic.utils :refer :all])
+  (:import datomic.Util))
