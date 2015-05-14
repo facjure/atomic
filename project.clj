1118f73f82e25785aa712c664f0207e5b778837e
Priyatam Mudivarti <priyatam@gmail.com>
2015-05-14 16:54:49 -0700

    added pro as default
    - peer and transactor should be of same type

--- project.clj
+++ project.clj
@@ -6,8 +6,8 @@
   :warn-on-reflection false
   :dependencies [[org.clojure/clojure "1.7.0-beta2"]
                  [environ "1.0.0"]
-                 [com.datomic/datomic-free "0.9.5153" :exclusions [joda-time]]
-                 ;[com.datomic/datomic-pro "0.9.5153" :exclusions [joda-time]]
+                 ;;[com.datomic/datomic-free "0.9.5153" :exclusions [joda-time]]
+                 [com.datomic/datomic-pro "0.9.5153" :exclusions [joda-time]]
                  [clj-time "0.9.0"]
                  [slingshot "0.12.2"]
                  [org.clojure/tools.logging "0.3.1"]]
