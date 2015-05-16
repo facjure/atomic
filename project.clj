1961c77db273cf737d7566987d04c6a6e678dc5e
Priyatam Mudivarti <priyatam@gmail.com>
2015-05-15 18:33:10 -0700

    up version, replaced datomic with free for tests
    - for unit tests

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject facjure/atomic "0.2.1"
+(defproject facjure/atomic "0.2.2"
   :description "A thin wrapper on Datomic"
   :url "https://github.com/facjure/atomic"
   :license {:name "Eclipse Public License"
@@ -10,8 +10,8 @@
   :warn-on-reflection false
   :dependencies [[org.clojure/clojure "1.7.0-beta2"]
                  [environ "1.0.0"]
-                 ;;[com.datomic/datomic-free "0.9.5153" :exclusions [joda-time]]
-                 [com.datomic/datomic-pro "0.9.5153" :exclusions [joda-time]]
+                 [com.datomic/datomic-free "0.9.5153" :exclusions [joda-time]]
+                 ;;[com.datomic/datomic-pro "0.9.5153" :exclusions [joda-time]]
                  [clj-time "0.9.0"]
                  [slingshot "0.12.2"]
                  [org.clojure/tools.logging "0.3.1"]]
