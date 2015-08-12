bfa53d2ff5b494775d63b777100ae5251d6e10e4
Priyatam Mudivarti <priyatam@gmail.com>
2015-08-12 13:45:10 -0700

    up ver

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject facjure/atomic "0.2.2"
+(defproject facjure/atomic "0.2.3"
   :description "A thin wrapper on Datomic"
   :url "https://github.com/facjure/atomic"
   :license {:name "Eclipse Public License"
@@ -9,17 +9,18 @@
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
   :warn-on-reflection false
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/tools.logging "0.3.1"]
                  [environ "1.0.0"]
-                 [com.datomic/datomic-free "0.9.5201" :scope  "provided"
+                 [com.datomic/datomic-free "0.9.5206" :scope  "provided"
                   :exclusions [joda-time
                                org.slf4j/slf4j-nop
                                org.slf4j/slf4j-log4j12]]
                  [ch.qos.logback/logback-classic "1.0.1"]
                  [clj-time "0.9.0"]
-                 [slingshot "0.12.2"]
-                 [org.clojure/tools.logging "0.3.1"]]
+                 [slingshot "0.12.2"]]
   :plugins [[lein-environ "1.0.0"]
             [lein-marginalia "0.8.0"]
             [lein-expectations "0.0.7"]
             [lein-autoexpect "1.4.2"]]
-  :profiles {:dev {:dependencies [[expectations "2.0.9"]]}})
+  :profiles {:dev {:dependencies [[org.clojure/tools.nrepl "0.2.10"]
+                                  [expectations "2.0.9"]]}})
