f9b7610ae79c03e77aa1f72e06e4d42150893f81
Priyatam Mudivarti <priyatam@gmail.com>
2016-01-24 23:14:26 -0800

    up deps, add schema

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject facjure/atomic "0.3.0"
+(defproject facjure/atomic "0.3.1"
   :description "A thin wrapper on Datomic"
   :url "https://github.com/facjure/atomic"
   :license {:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -6,12 +6,13 @@
   :min-lein-version "2.5.0"
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
   :warn-on-reflection false
-  :dependencies [[org.clojure/clojure "1.8.0-RC4"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.2.374"]
                  [org.clojure/core.typed "0.3.18"]
                  [org.clojure/tools.logging "0.3.1"]
                  [com.datomic/datomic-free "0.9.5344"]
                  [com.stuartsierra/component "0.3.1"]
+                 [prismatic/schema "1.0.4"]
                  [clj-time "0.11.0"]
                  [environ "1.0.1"]
                  [slingshot "0.12.2"]]
